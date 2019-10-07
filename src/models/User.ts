bf39ec26c838cdf7692090d27d5d7a67a4820452
Alexey <mr.oleksii5@gmail.com>
2019-10-07 18:14:27 +0300

    refactoring with composition

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
 
 interface UserProps {
     id?: number;
@@ -6,10 +6,8 @@ interface UserProps {
     age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-    events: { [key: string]: Callback[] } = {};
+    public events: Eventing = new Eventing();
 
     constructor(private data: UserProps) {}
 
@@ -19,30 +17,5 @@ export class User {
 
     set(update: UserProps): void {
         Object.assign(this.data, update);
-    }
-
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-
-        if(!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach(callback => {
-            callback();
-        })
-    }
-
-    fetch(): void {
-        axios.get(`http://localhost:3000/users/${this.get('id')}`)
-        .then((response: AxiosResponse): void => {
-            this.set(response.data);
-        });
     }
 }
